429f29db6475c67a848dd134d6d7c0e10fb33316
Leonard Petter <Leo@Leonards-MacBook-Pro.local>
2019-11-07 14:27:57 +0100

    [FIX] Added failing test correctly

--- src/test/java/org/bptlab/cepta/ParallelSuiteTest.java
+++ src/test/java/org/bptlab/cepta/ParallelSuiteTest.java
@@ -2,42 +2,38 @@ package org.bptlab.cepta;
 
 import org.testng.annotations.*;
 
-public class ParallelSuiteTest
-{
-    String testName = "";
+public class ParallelSuiteTest {
+  String testName = "";
 
-    @BeforeTest
-    @Parameters({ "test-name" })
-    public void beforeTest(String testName) {
-        this.testName = testName;
-        long id = Thread.currentThread().getId();
-        System.out.println("Before test " + testName + ". Thread id is: " + id);
-    }
+  @BeforeTest
+  @Parameters({"test-name"})
+  public void beforeTest(String testName) {
+    this.testName = testName;
+    long id = Thread.currentThread().getId();
+    System.out.println("Before test " + testName + ". Thread id is: " + id);
+  }
 
-    @BeforeClass
-    public void beforeClass() {
-        long id = Thread.currentThread().getId();
-        System.out.println("Before test-class " + testName + ". Thread id is: "
-                + id);
-    }
+  @BeforeClass
+  public void beforeClass() {
+    long id = Thread.currentThread().getId();
+    System.out.println("Before test-class " + testName + ". Thread id is: " + id);
+  }
 
-    @Test
-    public void testMethodOne() {
-        long id = Thread.currentThread().getId();
-        System.out.println("Sample test-method " + testName
-                + ". Thread id is: " + id);
-    }
+  @Test
+  public void testMethodOne() {
+    long id = Thread.currentThread().getId();
+    System.out.println("Sample test-method " + testName + ". Thread id is: " + id);
+  }
 
-    @AfterClass
-    public void afterClass() {
-        long id = Thread.currentThread().getId();
-        System.out.println("After test-method  " + testName
-                + ". Thread id is: " + id);
-    }
+  @AfterClass
+  public void afterClass() {
+    long id = Thread.currentThread().getId();
+    System.out.println("After test-method  " + testName + ". Thread id is: " + id);
+  }
 
-    @AfterTest
-    public void afterTest() {
-        long id = Thread.currentThread().getId();
-        System.out.println("After test  " + testName + ". Thread id is: " + id);
-    }
+  @AfterTest
+  public void afterTest() {
+    long id = Thread.currentThread().getId();
+    System.out.println("After test  " + testName + ". Thread id is: " + id);
+  }
 }
--- src/test/java/org/bptlab/cepta/RegularExpressionGroupTest.java
+++ src/test/java/org/bptlab/cepta/RegularExpressionGroupTest.java
@@ -3,30 +3,24 @@ package org.bptlab.cepta;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class RegularExpressionGroupTest
-{
-    @Test(groups = { "include-test-one" })
-    public void testMethodOne() {
-        System.out.println("Test method one");
-    }
+public class RegularExpressionGroupTest {
+  @Test(groups = {"include-test-one"})
+  public void testMethodOne() {
+    System.out.println("Test method one");
+  }
 
-    @Test(groups = { "include-test-two" })
-    public void testMethodTwo() {
-        System.out.println("Test method two");
-    }
+  @Test(groups = {"include-test-two"})
+  public void testMethodTwo() {
+    Assert.assertEquals(1, 2);
+  }
 
-    @Test(groups = { "test-one-exclude" })
-    public void testMethodThree() {
-        System.out.println("Test method three");
-    }
+  @Test(groups = {"test-one-exclude"})
+  public void testMethodThree() {
+    System.out.println("Test method three");
+  }
 
-    @Test(groups = { "test-two-exclude" })
-    public void testMethodFour() {
-        System.out.println("Test method Four");
-    }
-
-    @Test
-    public void testMethodFive() {
-        Assert.assertEquals('stop', 'test');
-    }
+  @Test(groups = {"test-two-exclude"})
+  public void testMethodFour() {
+    System.out.println("Test method Four");
+  }
 }
