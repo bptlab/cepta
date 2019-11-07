5ee1143b884f0dcbb80a88ff40497525fd74428b
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-11-07 15:12:01 +0100

    new files for TrainDataProducer - creator for TrainDataProducer - TrainData Class - De-/Serializer - not finished; in work

---
+++ src/main/java/connectors/consumer_producer/ProducerCreator.java
@@ -0,0 +1,36 @@
+package connectors.consumer_producer;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import org.apache.kafka.clients.producer.Producer;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import org.apache.kafka.common.serialization.LongSerializer;
+
+import org.apache.kafka.common.serialization.StringSerializer;
+
+
+public class ProducerCreator {
+    // this class can create a producer with the config stuff from the KafkaConstants file
+    // more at https://dzone.com/articles/kafka-producer-and-consumer-example
+    public static Producer<Long, String> createProducer() {
+
+        Properties props = new Properties();
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstants.KAFKA_BROKERS);
+
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, KafkaConstants.CLIENT_ID);
+
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, TrainDataSerializer.class.getName());
+
+        //props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class.getName());
+
+        return new KafkaProducer<>(props);
+
+    }
+
+}
---
+++ src/main/java/connectors/consumer_producer/TrainDataSerializer.java
@@ -0,0 +1,43 @@
+package connectors.consumer_producer;
+
+import java.util.Map;
+
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Serializer;
+
+public class TrainDataSerializer implements Serializer<TrainData> {
+    // serializes TrainData
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+
+    }
+
+    @Override
+    public byte[] serialize(String topic, TrainData data) {
+
+        byte[] retVal = null;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+
+            retVal = objectMapper.writeValueAsString(data).getBytes();
+
+        } catch (Exception exception) {
+
+            System.out.println("Error in serializing object"+ data);
+
+        }
+
+        return retVal;
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+
+}
