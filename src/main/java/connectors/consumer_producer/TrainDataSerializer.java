134d1218a470f590e02ebb5cda2f1a725f727cd1
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-11-11 18:03:22 +0100

    serializer should work - I have no Idea how to make sure it works - only serializer works; not the deserializer (with consumer)

--- src/main/java/connectors/consumer_producer/TrainDataSerializer.java
+++ src/main/java/connectors/consumer_producer/TrainDataSerializer.java
@@ -1,7 +1,15 @@
 package connectors.consumer_producer;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.io.*;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.kafka.common.serialization.Serializer;
 import org.codehaus.jackson.map.ObjectMapper;
 
@@ -14,7 +22,9 @@ public class TrainDataSerializer implements Serializer<TrainData> {
 
     @Override
     public byte[] serialize(String arg0, TrainData data) {
+/*
         byte[] retVal = null;
+        BinaryEncoder encoder = null;
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
@@ -24,7 +34,21 @@ public class TrainDataSerializer implements Serializer<TrainData> {
         }
 
         return retVal;
-
+*/
+        try {
+            Schema schema = new Schema.Parser().parse(new File("/src/main/avro/trainData.avsc"));
+            DatumReader<Object> reader = new GenericDatumReader<>(schema);
+            GenericDatumWriter<Object> writer = new GenericDatumWriter<>(schema);
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            Decoder decoder = DecoderFactory.get().jsonDecoder(schema, data.toString());
+            Encoder encoder = EncoderFactory.get().binaryEncoder(output, null);
+            Object datum = reader.read(null, decoder);
+            writer.write(datum, encoder);
+            encoder.flush();
+            return output.toByteArray();
+        } catch (IOException e){
+        }
+    return null;
     }
 
     @Override
