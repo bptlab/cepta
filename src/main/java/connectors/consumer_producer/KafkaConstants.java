06ca9838d3a8d6ed9ba58fd1b77a5ad52f497e75
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-11-19 12:02:51 +0100

    re,ove unnecessary constants

--- src/main/java/connectors/consumer_producer/KafkaConstants.java
+++ src/main/java/connectors/consumer_producer/KafkaConstants.java
@@ -3,19 +3,10 @@ package connectors.consumer_producer;
 public interface KafkaConstants {
     String KAFKA_BROKERS = "localhost:9092";
 
-    Integer MESSAGE_COUNT = 1000;
-
     String CLIENT_ID = "client1";
 
     String TOPIC_NAME = "test";
 
     String GROUP_ID_CONFIG = "consumerGroup1";
 
-    Integer MAX_NO_MESSAGE_FOUND_COUNT = 100;
-
-    String OFFSET_RESET_LATEST = "latest";
-
-    String OFFSET_RESET_EARLIER = "earliest";
-
-    Integer MAX_POLL_RECORDS = 1;
 }
