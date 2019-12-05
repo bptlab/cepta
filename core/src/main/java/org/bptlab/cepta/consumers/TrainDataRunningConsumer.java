3a726dfc4c555535af9f62518b3371b6e3aecf1a
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2019-12-05 08:41:20 +0100

    Refactor kafka consumers

---
+++ core/src/main/java/org/bptlab/cepta/consumers/TrainDataRunningConsumer.java
@@ -0,0 +1,48 @@
+package org.bptlab.cepta.consumers;
+
+import java.sql.Connection;
+import java.util.Collections;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.bptlab.cepta.LiveTrainData;
+
+public class TrainDataRunningConsumer extends Consumer {
+
+  private boolean connected = false;
+  private Connection connection;
+
+  TrainDataRunningConsumer(Properties props) {
+    super(props);
+    subscribe(Collections.singletonList(topic));
+  }
+
+  public void shutdown() {
+    close();
+    // logger.info("Consumer shutting down");
+  }
+
+  public void start() {
+    this.running = true;
+
+    while (true) {
+      final ConsumerRecords<Long, LiveTrainData> consumerRecords = poll(timeout);
+      if (consumerRecords.count() < 1) {
+        // logger.warn(String.format("Did not receive any records in %d milliseconds", timeout));
+        continue;
+      }
+      consumerRecords.forEach(
+          record -> {
+            /* logger.debug(
+            String.format(
+                "Consumer Record:(%d, %s, %d, %d)\n",
+                record.key(), record.value(), record.partition(), record.offset())); */
+          });
+      commitAsync();
+    }
+  }
+
+  public void stop() {
+    this.running = false;
+    // logger.info("Consumer stopping");
+  }
+}
