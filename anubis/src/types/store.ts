b6cff052fcc4a8da97b0d9edc2b86223df3e7116
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2019-12-08 06:49:37 +0100

    Combined type stub definitions

--- anubis/src/types/store.ts
+++
@@ -1,9 +0,0 @@
-export interface RootState {
-  appName: string;
-  appAllowsRegister: boolean;
-  appAuthenticationAPI: string;
-  appSignUpAPI: string;
-  isCollapsed: boolean;
-  authToken: string;
-  authStatus: string;
-}
