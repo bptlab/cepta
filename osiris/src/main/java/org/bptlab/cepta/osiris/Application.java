cbf0b80fa1acf37275d836a77affafd28a03271d
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2019-12-09 05:05:06 +0100

    Start replayer service

--- osiris/src/main/java/org/bptlab/cepta/osiris/Application.java
+++ osiris/src/main/java/org/bptlab/cepta/osiris/Application.java
@@ -1,5 +1,7 @@
 package org.bptlab.cepta.osiris;
 
+import org.bptlab.cepta.producers.replayer.Success;
+import org.bptlab.cepta.schemas.grpc.ReplayerClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -9,6 +11,13 @@ import org.springframework.context.annotation.PropertySource;
 public class Application {
 
 	public static void main(String[] args) {
+
+		// Start the replayer
+		ReplayerClient test = new ReplayerClient("localhost", 9005);
+		try {
+			Success success = test.start();
+		} catch (InterruptedException ex) {}
+
 		SpringApplication.run(Application.class, args);
 	}
 
