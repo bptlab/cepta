28aff627f0e0ceb1338ebed4cf86212b88305499
Chris Laptop <christopher.schulz@student.hpi.de>
2019-12-19 13:57:33 +0100

    Refactores LiveTrainDataReplayer to be the same as Planned TrainDataReplayer using LiveTrainDataDataBaseConverter

--- producers/train-replayer/src/main/java/org/bptlab/cepta/producers/trainreplayer/LiveTrainDataReplayer.java
+++ producers/train-replayer/src/main/java/org/bptlab/cepta/producers/trainreplayer/LiveTrainDataReplayer.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.util.Properties;
 import org.bptlab.cepta.LiveTrainData;
 import org.bptlab.cepta.producers.PostgresReplayer;
+import org.bptlab.cepta.utils.converters.LiveTrainDataDatabaseConverter;
 
 public class LiveTrainDataReplayer extends PostgresReplayer<Long, LiveTrainData> {
 
@@ -15,27 +16,6 @@ public class LiveTrainDataReplayer extends PostgresReplayer<Long, LiveTrainData>
 
   @Override
   public LiveTrainData convertToEvent(ResultSet result) throws Exception {
-    LiveTrainData.Builder event = LiveTrainData.newBuilder();
-    try {
-      event.setId(result.getInt("id"));
-      event.setTrainId(result.getInt("train_id"));
-      event.setLocationId(result.getInt("location_id"));
-      convertTimestamp(result.getTimestamp("actual_time"), event::setActualTime);
-      event.setStatus(result.getInt("status"));
-      event.setFirstTrainNumber(result.getInt("first_train_number"));
-      event.setTrainNumberReference(result.getInt("train_number_reference"));
-      convertTimestamp(result.getTimestamp("arrival_time_reference"), event::setArrivalTimeReference);
-      event.setPlannedArrivalDeviation(result.getInt("planned_arrival_deviation"));
-      event.setTransferLocationId(result.getInt("transfer_location_id"));
-      event.setReportingImId(result.getInt("reporting_im_id"));
-      event.setNextImId(result.getInt("next_im_id"));
-      event.setMessageStatus(result.getInt("message_status"));
-      convertTimestamp(result.getTimestamp("message_creation"), event::setMessageCreation);
-    } catch (Exception e) {
-      logger.error("Failed to convert database entry to live train data event");
-      throw e;
-    }
-    logger.debug(event.build().toString());
-    return event.build();
+    return new LiveTrainDataDatabaseConverter().fromResult(result);
   }
 }
