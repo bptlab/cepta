a2be54b6cda926dd2f187ee9cf210fb216398a04
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-01-07 02:32:54 +0100

    Initial prod env

---
+++ deployment/prod/devenv.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+cd compose
+# -f postgresimporter.compose.yml
+docker-compose \
+  -f core.compose.yml \
+  -f cockpit.compose.yml \
+  -f envoy.compose.yml \
+  -f anubis.compose.yml \
+  -f rabbitmq.compose.yml \
+  -f monitoring.compose.yml \
+  "$@"
