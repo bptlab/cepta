3b280522baaccbc8bb087469b7ca90d9bc5a156b
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-01-07 15:17:42 +0100

    [FIX] Proxy both websockets and grpc with envoy

--- deployment/dev/prepare-envoy.sh
+++ deployment/dev/prepare-envoy.sh
@@ -3,4 +3,5 @@ apt install -y gettext-base
 # $CEPTA_ENVOY_INGRESS_PORT,$CEPTA_ENVOY_GRPC_PORT
 cat /etc/envoy/envoy.yaml.tmpl | envsubst > /etc/envoy/envoy.yaml
 echo "Did substitute config file"
+cat /etc/envoy/envoy.yaml
 exec /docker-entrypoint.sh "$@"
