d99e7396eae593aca08c9be2019563eb243f91b1
Vincent <vincent.opitz@student.hpi.de>
2020-01-14 13:26:18 +0100

    [#49][IMP] refactoring

--- core/src/main/java/org/bptlab/cepta/operators/WeatherLiveTrainJoinFunction.java
+++ core/src/main/java/org/bptlab/cepta/operators/WeatherLiveTrainJoinFunction.java
@@ -1,11 +1,10 @@
 package org.bptlab.cepta.operators;
 
-import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.common.functions.RichJoinFunction;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
-import org.apache.flink.streaming.api.windowing.assigners.SlidingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.bptlab.cepta.LiveTrainData;
 import org.bptlab.cepta.TrainDelayNotification;
@@ -14,30 +13,26 @@ import org.bptlab.cepta.WeatherData;
 public class WeatherLiveTrainJoinFunction {
   public static DataStream<TrainDelayNotification> delayFromWeather(DataStream<Tuple2<WeatherData, Integer>> weather, DataStream<LiveTrainData> train){
     return weather.join(train)
-        .where(new KeySelector<Tuple2<WeatherData, Integer>, Object>() {
+        .where(new KeySelector<Tuple2<WeatherData, Integer>, Integer>() {
           @Override
-          public Object getKey(Tuple2<WeatherData, Integer> weatherDataIntegerTuple2)
+          public Integer getKey(Tuple2<WeatherData, Integer> weatherDataIntegerTuple2)
               throws Exception {
-            System.out.println(weatherDataIntegerTuple2);
             return weatherDataIntegerTuple2.f1;
           }
-        }).equalTo(new KeySelector<LiveTrainData, Object>() {
+        }).equalTo(new KeySelector<LiveTrainData, Integer>() {
           @Override
-          public Object getKey(LiveTrainData liveTrainData) throws Exception {
-            System.out.println(liveTrainData.getLocationId());
+          public Integer getKey(LiveTrainData liveTrainData) throws Exception {
             return liveTrainData.getLocationId();
           }
-        })//.window(SlidingEventTimeWindows.of(Time.seconds(60), Time.seconds(5)))
-        .window(SlidingProcessingTimeWindows.of(Time.seconds(60), Time.seconds(10)))
-        .apply(new JoinFunction<Tuple2<WeatherData, Integer>, LiveTrainData, TrainDelayNotification>() {
+        })
+        .window(SlidingEventTimeWindows.of(Time.seconds(60), Time.seconds(2)))
+        .apply(new RichJoinFunction<Tuple2<WeatherData, Integer>, LiveTrainData, TrainDelayNotification>() {
           @Override
           public TrainDelayNotification join(Tuple2<WeatherData, Integer> weatherDataIntegerTuple2,
               LiveTrainData liveTrainData) throws Exception {
-
             TrainDelayNotification adelheit = TrainDelayNotification.newBuilder().setDelay(9000)
                 .setTrainId(liveTrainData.getTrainId()).setLocationId(liveTrainData.getLocationId())
                 .build();
-            System.out.println(adelheit);
             return adelheit;
           }
         });
