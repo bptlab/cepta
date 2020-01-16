e22324fb5e476efd77af5268a208130fef50d449
Leonard Petter <leonard.petter@student.hpi.de>
2020-01-16 12:00:55 +0100

    Introduced further go websocket service functionality

--- osiris/websockets/go.mod
+++ osiris/websockets/go.mod
@@ -2,4 +2,7 @@ module websockets
 
 go 1.13
 
-require github.com/gorilla/websocket v1.4.1 // indirect
+require (
+	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/linkedin/goavro/v2 v2.9.7 // indirect
+)
--- osiris/websockets/main.go
+++ osiris/websockets/main.go
@@ -1,75 +1,102 @@
 package main
 
 import (
+	"log"
+	"net/http"
   "fmt"
-  "log"
-  "net/http"
-  "github.com/gorilla/websocket"
+  "github.com/linkedin/goavro/v2"
+	"github.com/gorilla/websocket"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Home Page")
+/*
+type Traindata struct {
+	StationID int    `json:"id"`
+	Station   string `json:"station"`
+	oldETA    string `json:"old"`
+	Delay     string `json:"delay"`
+	Cause     string `json:"cause"`
+	newETA    string `json:"new"`
+}
+*/
+
+
+// Need to define an Upgrade
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-  // remove the previous fmt statement
-  // fmt.Fprintf(w, "Hello World")
-  // Allow all incoming connections
-  upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	// Allow all incoming connections
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
-  // upgrade this connection to a WebSocket
-  // connection
-  ws, err := upgrader.Upgrade(w, r, nil)
-  if err != nil {
-    log.Println(err)
-  }
+	// upgrade this connection to a WebSocket
+	// connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
 
-  log.Println("Client Connected")
-  err = ws.WriteMessage(1, []byte("Hi Client!"))
-  if err != nil {
-    log.Println(err)
-  }
-  // listen indefinitely for new messages coming
-  // through on our WebSocket connection
-  reader(ws)
-}
+	log.Println("Client Connected")
+	if err != nil {
+		log.Println(err)
+	}
 
-// We'll need to define an Upgrader
-// this will require a Read and Write buffer size
-var upgrader = websocket.Upgrader{
-  ReadBufferSize:  1024,
-  WriteBufferSize: 1024,
+	// listen indefinitely for new messages coming
+	// through on our WebSocket connection
+	reader(ws)
 }
 
 // define a reader which will listen for
 // new messages being sent to our WebSocket
 // endpoint
 func reader(conn *websocket.Conn) {
-  for {
-    // read in a message
-    messageType, p, err := conn.ReadMessage()
-    if err != nil {
-      log.Println(err)
-      return
-    }
-    // print out that message for clarity
-    fmt.Println(string(p))
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+		  log.Println(err)
+		  return
+		}
 
-    if err := conn.WriteMessage(messageType, p); err != nil {
-      log.Println(err)
-      return
-    }
+    /*
+    codec, err := goavro.NewCodec(`
+              {
+                "namespace": "org.bptlab.cepta",
+                "type": "record",
+                "name": "TrainDelayNotification",
+                "fields": [
+                  {"name": "train_id", "type": "int"},
+                  {"name": "location_id", "type": "int"},
+                  {"name": "delay", "type": "string"}
+                  ]
+              }
+              `)
+    */
+     
+    textual := []byte(`{"train_id":"10", "location_id":"12", "delay":"30min"}`)
 
-  }
+    // Convert textual Avro data (in Avro JSON format) to native Go form
+    native, _, err := codec.NativeFromTextual(textual)
+    fmt.Println(native)
+
+		// send data to the client
+		conn.WriteMessage(1, []byte(`{"next":{"LongList":{}}}`))
+
+		if err != nil {
+			log.Println(string(messageType))
+			log.Println(string(p))
+			return
+		}
+
+	}
 }
 
 func setupRoutes() {
-  http.HandleFunc("/", homePage)
-  http.HandleFunc("/ws", wsEndpoint)
+	http.HandleFunc("/ws", wsEndpoint)
 }
 
 func main() {
-  fmt.Println("Hello World")
-  setupRoutes()
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":5555", nil))
 }
