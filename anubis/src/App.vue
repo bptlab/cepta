9c152946770c57f621fbf6cd3ffc3e19d835d1cd
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2020-01-16 11:26:31 +0100

    [#79] Fix App.vue

--- anubis/src/App.vue
+++ anubis/src/App.vue
@@ -7,158 +7,157 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import NprogressContainer from "vue-nprogress/src/NprogressContainer.vue";
-import SockJS from "sockjs-client";
+<script>
+  import NprogressContainer from "vue-nprogress/src/NprogressContainer";
+  import SockJS from "sockjs-client";
 
-
-@Component({
-  name: "App",
-  components: {
-    NprogressContainer
-  }
-})
-export default class App extends Vue {
-  socket = new SockJS("http://localhost:5000/ws");
-
-  redraw() {
-    this.$redrawVueMasonry();
-  }
-
-  connectWebsocket() {
-    this.$store.commit('setWebsocket', this.socket);
-  }
-
-  created() {
-    this.axios.interceptors.response.use(undefined, function(err) {
-      return new Promise(function(resolve, reject) {
-        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
-          // if you ever get an unauthorized, logout the user
-          this.$store.dispatch("AUTH_LOGOUT");
-          // you can also redirect to /login if needed !
-          this.$router.push("/login");
-        }
-        throw err;
+  export default {
+    name: "App",
+    components: {
+      NprogressContainer
+    },
+    props: {},
+    data() {
+      return {};
+    },
+    computed: {},
+    methods: {
+      reDraw: function() {
+        this.$redrawVueMasonry();
+      },
+      connectWebsocket() {
+        this.socket = new SockJS("http://localhost:5000/ws");
+        this.$store.commit('setWebsocket', this.socket);
+      }
+    },
+    created() {
+      this.axios.interceptors.response.use(undefined, function(err) {
+        return new Promise(function(resolve, reject) {
+          if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+            // if you ever get an unauthorized, logout the user
+            this.$store.dispatch("AUTH_LOGOUT");
+            // you can also redirect to /login if needed !
+            this.$router.push("/login");
+          }
+          throw err;
+        });
       });
-    });
-  }
+    },
+    mounted() {
+      this.$redrawVueMasonry();
+      this.connectWebsocket();
+    },
+    destroyed() {
+      this.socket.close();
+    }
 
-  mounted() {
-    this.redraw();
-    this.connectWebsocket();
-  }
-
-  destroyed() {
-    this.socket.close();
-  }
-}
+  };
 </script>
 
 <style lang="scss">
-@import "~bootstrap/scss/bootstrap";
-@font-face {
-  font-family: "themify";
-  src: url(~themify-icons/themify-icons/fonts/themify.eot?-fvbane);
-  src: url(~themify-icons/themify-icons/fonts/themify.eot?#iefix-fvbane)
-  format("embedded-opentype"),
-  url(~themify-icons/themify-icons/fonts/themify.woff?-fvbane) format("woff"),
-  url(~themify-icons/themify-icons/fonts/themify.ttf?-fvbane)
-  format("truetype"),
-  url(~themify-icons/themify-icons/fonts/themify.svg?-fvbane#themify)
-  format("svg");
-  font-weight: normal;
-  font-style: normal;
-}
-@import "~themify-icons/themify-icons/variables";
-@import "~themify-icons/themify-icons/mixins";
-@import "~themify-icons/themify-icons/core";
-@import "~themify-icons/themify-icons/extras";
-@import "~themify-icons/themify-icons/icons";
+  @import "~bootstrap/scss/bootstrap";
+  @font-face {
+    font-family: "themify";
+    src: url(~themify-icons/themify-icons/fonts/themify.eot?-fvbane);
+    src: url(~themify-icons/themify-icons/fonts/themify.eot?#iefix-fvbane)
+    format("embedded-opentype"),
+    url(~themify-icons/themify-icons/fonts/themify.woff?-fvbane) format("woff"),
+    url(~themify-icons/themify-icons/fonts/themify.ttf?-fvbane)
+    format("truetype"),
+    url(~themify-icons/themify-icons/fonts/themify.svg?-fvbane#themify)
+    format("svg");
+    font-weight: normal;
+    font-style: normal;
+  }
+  @import "~themify-icons/themify-icons/variables";
+  @import "~themify-icons/themify-icons/mixins";
+  @import "~themify-icons/themify-icons/core";
+  @import "~themify-icons/themify-icons/extras";
+  @import "~themify-icons/themify-icons/icons";
 </style>
 
 <style lang="sass">
-@import "@/style/custom.sass"
+  @import "/style/custom.sass"
 
-@import "@/style/spec/index.sass"
-@import "@/style/vendor/index.sass"
+  @import "/style/spec/index.sass"
+  @import "/style/vendor/index.sass"
 
-.ps__rail-y
-  right: 0 !important
-  left: auto !important
+  .ps__rail-y
+    right: 0 !important
+    left: auto !important
 
-.nprogress-container
-  height: 3px
-  top: 0
-  z-index: 9999999
-  position: fixed
-  width: 100%
+  .nprogress-container
+    height: 3px
+    top: 0
+    z-index: 9999999
+    position: fixed
+    width: 100%
 
-#nprogress .bar
-  background: #42b983
-  height: 3px
+  #nprogress .bar
+    background: #42b983
+    height: 3px
 
-/* Base */
-html, html a, body
-  -webkit-font-smoothing: antialiased
-
-a
-  transition: all 0.3s ease-in-out
-
-body
-  font-family: $font-primary
-  font-size: 14px
-  color: $default-text-color
-  line-height: 1.5
-  letter-spacing: 0.2px
-  overflow-x: hidden
-h1,
-h2,
-h3,
-h4,
-h5,
-h6
-  font-family: $font-secondary
-  letter-spacing: 0.5px
-  line-height: 1.5
+  /* Base */
+  html, html a, body
+    -webkit-font-smoothing: antialiased
 
   a
+    transition: all 0.3s ease-in-out
+
+  body
+    font-family: $font-primary
+    font-size: 14px
+    color: $default-text-color
+    line-height: 1.5
+    letter-spacing: 0.2px
+    overflow-x: hidden
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6
     font-family: $font-secondary
+    letter-spacing: 0.5px
+    line-height: 1.5
 
-  small
-    font-weight: 300
-    color: lighten($default-dark, 5%)
+    a
+      font-family: $font-secondary
 
-p
-  font-family: $font-primary
-  line-height: 1.9
+    small
+      font-weight: 300
+      color: lighten($default-dark, 5%)
 
-.lead
-  font-size: 18px
+  p
+    font-family: $font-primary
+    line-height: 1.9
 
-ul
-  margin-bottom: 0
+  .lead
+    font-size: 18px
 
-a
-  color: $default-info
+  ul
+    margin-bottom: 0
 
-  &:hover,
-  &:focus
-    text-decoration: none
-    color: darken($default-info, 10%)
+  a
+    color: $default-info
 
-  &:focus
+    &:hover,
+    &:focus
+      text-decoration: none
+      color: darken($default-info, 10%)
+
+    &:focus
+      outline: none
+
+    &.text-gray
+      &:hover,
+      &:focus,
+      &.active
+        color: $default-dark !important
+
+  \:focus
     outline: none
 
-  &.text-gray
-    &:hover,
-    &:focus,
-    &.active
-      color: $default-dark !important
-
-\:focus
-  outline: none
-
-hr
-  border-top: 1px solid $border-color
+  hr
+    border-top: 1px solid $border-color
 </style>
