ee69ba30fdf6d771051b94714fb20dd486ccd9ee
Leonard Petter <leonard.petter@student.hpi.de>
2020-01-20 22:22:41 +0100

    [IMP] Work with the stream connection between kafka and the websockets

--- osiris/websockets/go.mod
+++ osiris/websockets/go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/Shopify/sarama v1.25.0 // indirect
-	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/gorilla/websocket v1.4.1
 	github.com/linkedin/goavro/v2 v2.9.7 // indirect
 	github.com/segmentio/kafka-go v0.3.5 // indirect
 )
---
+++ osiris/websockets/websocket/websocket.go
@@ -0,0 +1,24 @@
+package websocket
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return conn, nil
+}
