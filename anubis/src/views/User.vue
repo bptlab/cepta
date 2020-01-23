d4ef45d48cfa43616ba85faabe8aedcd6653a3e7
Vincent <vincent.opitz@student.hpi.de>
2020-01-23 16:06:48 +0100

    [#104][FEAT] add second thingy in layout

--- anubis/src/views/User.vue
+++ anubis/src/views/User.vue
@@ -1,19 +1,34 @@
 <template>
-  <div>
-    <train-id-list/>
-    <p>
-      Hey hey hey :)
-    </p>
-  </div>
+  <masonry-layout title="Your trainIDs">
+  <masonry-layout-tile section="some Section">
+    <input ref="search" class="form-control" id="trainID oder so" type="number" placeholder="search ... ;)">
+      <train-id-info/>
+    </masonry-layout-tile>
+    <masonry-layout-tile section="hallo Section">
+      <basic-table
+        :table-data="trainIDs"
+        :show-indices="false"
+        :striped="true"
+        :bordered="true"
+        cellspacing="0"
+        :data-table="true"
+      />
+    </masonry-layout-tile>
+  </masonry-layout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import TrainIdList from "../components/TrainIdList.vue";
+import MasonryLayout from "../components/MasonryLayout.vue";
+import MasonryLayoutTile from "../components/MasonryLayoutTile.vue";
 
 @Component({
   name: "user",
-  components: { TrainIdList },
+  components: { 
+    TrainIdList,
+    MasonryLayout,
+    MasonryLayoutTile },
   props: {}
 })
 export default class User extends Vue {
