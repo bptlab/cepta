1a17834c46a45d74b3d26f76570ea0d4bd654209
Gritta Weisheit <gritta.weisheit@giz.berlin>
2020-01-27 12:11:03 +0100

    add id table and computed section name

--- anubis/src/views/User.vue
+++ anubis/src/views/User.vue
@@ -1,19 +1,19 @@
 <template>
-  <masonry-layout title="Your trainIDs">
-  <masonry-layout-tile section="some Section">
+  <masonry-layout title="Your trains">
+  <masonry-layout-tile section="IDs">
     <input ref="search" class="form-control" id="trainID oder so" type="number" placeholder="search ... ;)">
-      <train-id-info/>
-    </masonry-layout-tile>
-    <masonry-layout-tile section="hallo Section">
       <basic-table
         :table-data="trainIDs"
         :show-indices="false"
         :striped="true"
         :bordered="true"
         cellspacing="0"
-        :data-table="true"
+        :data-table="false"
       />
     </masonry-layout-tile>
+  <masonry-layout-tile v-bind:section=sectionTitle id="sectionTitle">
+
+  </masonry-layout-tile>
   </masonry-layout>
 </template>
 
@@ -22,19 +22,27 @@ import { Component, Vue } from "vue-property-decorator";
 import TrainIdList from "../components/TrainIdList.vue";
 import MasonryLayout from "../components/MasonryLayout.vue";
 import MasonryLayoutTile from "../components/MasonryLayoutTile.vue";
+import BasicTable from "../components/BasicTable.vue";
 
 @Component({
   name: "user",
   components: { 
     TrainIdList,
     MasonryLayout,
-    MasonryLayoutTile },
+    MasonryLayoutTile,
+    BasicTable },
   props: {}
 })
 export default class User extends Vue {
-  example: string = "Test";
+  example: string = "Test";;
+  trainIDs:Array<Array<number>> = [[1], [2], [3], [4]];
   mounted() {}
+
+  get sectionTitle() {
+    return 10
+  }
 }
+
 </script>
 
 <style lang="sass"></style>
