5e2b2c2034500bbdf9dabd5a85c33882db937ca9
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2020-01-28 13:29:06 +0100

    Changed authentication microservice to our needs

---
+++ osiris/microservices/authentication/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"authentication/app"
+	"authentication/controllers"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+
+	router.Use(app.JwtAuthentication) //attach JWT auth middleware
+
+	//router.NotFoundHandler = app.NotFoundHandler
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
+}
