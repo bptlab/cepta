c6a0020fe8d2b7bee907bda443a390611f9ebc95
Gritta Weisheit <gritta.weisheit@giz.berlin>
2020-02-03 14:12:50 +0100

    start embedding Replayer in live train replayer

--- auxiliary/producers/traindataproducer/LiveTrainDataReplayer.go
+++ auxiliary/producers/traindataproducer/LiveTrainDataReplayer.go
@@ -9,48 +9,18 @@ import (
 	pb "github.com/bptlab/cepta/models/grpc/replayer"
 	libdb "github.com/bptlab/cepta/osiris/lib/db"
 	kafkaproducer "github.com/bptlab/cepta/osiris/lib/kafka/producer"
+	replayer "github.com/bptlab/cepta/auxiliary/producers/replayer/replayer"
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/jinzhu/gorm"
 	"github.com/sirupsen/logrus"
 )
 
-type Replayer struct {
-	TableName  string
-	MustMatch  *[]string
-	Timerange  *pb.Timerange
-	SortColumn string
-	Limit      *int
-	Speed      *int
-	Mode       *pb.ReplayType
-	Offset     int
-	Db         *libdb.DB
-	Brokers    []string
-	log        *logrus.Entry
-	running    bool
-	producer   *kafkaproducer.KafkaProducer
+type LiveTrainReplayer struct {
+	replayer.Replayer
 }
 
-func buildQuery(column string, timerange *pb.Timerange) []string {
-	frmt := "2006-01-02 15:04:05" // Go want's this date!
-	query := []string{}
-	if start, err := ptypes.Timestamp(timerange.GetStart()); err != nil && start.Unix() > 0 {
-		query = append(query, fmt.Sprintf("%s >= '%s'", column, start.Format(frmt)))
-	}
-	if end, err := ptypes.Timestamp(timerange.GetEnd()); err != nil && end.Unix() > 0 {
-		query = append(query, fmt.Sprintf("%s < '%s'", column, end.Format(frmt)))
-	}
-	return query
-}
-
-func DebugDatabase(r *Replayer) *gorm.DB {
-	if r.log.Logger.IsLevelEnabled(logrus.DebugLevel) {
-		return r.Db.DB.Debug()
-	}
-	return r.Db.DB
-}
-
-func (r Replayer) produce() error {
+func (r LiveTrainReplayer) produce() error {
 	query := DebugDatabase(&r).Model(&libdb.LiveTrainData{})
 	// Match ERRIDs
 	if r.MustMatch != nil {
@@ -119,26 +89,4 @@ func (r Replayer) produce() error {
 		}
 	}
 	return err
-}
-
-func (r Replayer) Start(log *logrus.Logger) error {
-	r.log = log.WithField("source", r.TableName)
-	r.log.Info("Starting to produce")
-	brokerList := []string{"localhost:29092"}
-	var err error
-	r.producer, err = kafkaproducer.KafkaProducer{}.ForBroker(brokerList)
-	if err != nil {
-		log.Warnf("Failed to start kafka producer: %s", err.Error())
-		log.Fatal("Cannot produce events")
-	}
-	defer func() {
-		if err := r.producer.Close(); err != nil {
-			r.log.Println("Failed to close server", err)
-		}
-	}()
-	err = r.produce()
-	if err != nil {
-		panic(err)
-	}
-	return nil
 }
