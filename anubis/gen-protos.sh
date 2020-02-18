b88a121062d342daf7a62f446096cd2b1b45b2cc
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-02-18 10:49:04 +0100

    Update multi stage docker build paths

--- anubis/gen-protos.sh
+++ anubis/gen-protos.sh
@@ -2,23 +2,26 @@
 
 set -e
 
-cd $(dirname $0)
-
+DIR=$(dirname $0)
+cd $DIR
 cmd="$@"
 
-# Build all models
-# ../run.sh build //models
-if [ ! -z "$SKIPPROTOCOMPILATION" ]
+if [ -z "$SKIPPROTOCOMPILATION" ]
 then
+    # Build all models
+    # ../run.sh build //models
+    echo "Compiling protos"
     bazel build //models
 
     # Now copy all generated typescript files
     # find ../bazel-bin/models/ -type f -path '**/models/**/*.js'
     # find ../bazel-bin/models/ -type f -path '**/models/**/*.ts'
-    rm ./src/generated/protobuf/*
-    find ../bazel-bin/models/ -type f -path '**/models/**/*.ts' -exec /bin/cp -rf '{}' ./src/generated/protobuf ';'
-    find ../bazel-bin/models/ -type f -path '**/models/**/*.js' -exec /bin/cp -rf '{}' ./src/generated/protobuf ';'
-    chmod 777 -R ./src/generated/protobuf
+    rm -f $DIR/src/generated/protobuf
+    echo "Creating $DIR/src/generated/protobuf"
+    mkdir -p $DIR/src/generated/protobuf
+    find ../bazel-bin/models/ -type f -path '**/models/**/*.ts' -exec /bin/cp -rf '{}' $DIR/src/generated/protobuf ';'
+    find ../bazel-bin/models/ -type f -path '**/models/**/*.js' -exec /bin/cp -rf '{}' $DIR/src/generated/protobuf ';'
+    chmod 777 -R $DIR/src/generated/protobuf
 fi
 
 exec $cmd
