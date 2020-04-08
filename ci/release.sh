6af88eaf22bbd0f4e2b833ed48f0e882adee55a0
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-04-08 18:34:44 +0200

    Implement test and publish stages

--- ci/release.sh
+++ ci/release.sh
@@ -5,15 +5,19 @@ cd ../
 
 echo "Preparing for release"
 
-# Assure all (including internal) tests succeed
+# Assure all internal tests succeed
 echo "Running tests and checks"
+bazel test :internal --test_output=errors
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 # Bump the version
-echo "Incrementing tbe version"
+echo "Incrementing the version"
 bump2version "$@"
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-# Build and push the docker containers (reuquired docker registry access)
-echo "Building docker containers"
-echo "Publishing to docker hub"
+# Build and push the docker containers (requires docker registry access)
+echo "Building and publishing docker images to docker hub"
+bazel run :publish
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 echo "Release completed"
