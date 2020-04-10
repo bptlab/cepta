1498cf093a33d55c895dd8f8e19e2c97c6801fbc
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-04-10 10:51:14 +0200

    Theme error pages

--- anubis/src/views/404.vue
+++ anubis/src/views/404.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="error-container">
-    <div class="error-image">
-      <img alt="" src="../assets/images/404.png" />
-    </div>
-
     <div class="error">
       <h1 class="mB-30">404</h1>
-      <h3 class="mB-10">Oops Page Not Found</h3>
+      <h3 class="mB-10">Page Not Found</h3>
       <p class="mB-30">
         The page you are looking for does not exist or has been moved.
       </p>
@@ -28,9 +24,12 @@ export default class ErrorPage404 extends Vue {
 
 <style lang="sass">
 .error-container
+  position: relative
   .error-image
     margin-right: 60px
   .error
+    width: 100%
+    text-align: center
     h1
       font-size: 60px
       font-weight: 900
