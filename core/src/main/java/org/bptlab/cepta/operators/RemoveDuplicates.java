dc77226a2fba3c65f9189fc021e08886827375a5
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2020-04-13 15:32:41 +0200

    fixed stream and commented Pattern implementation

--- core/src/main/java/org/bptlab/cepta/operators/RemoveDuplicates.java
+++ core/src/main/java/org/bptlab/cepta/operators/RemoveDuplicates.java
@@ -2,13 +2,13 @@ package org.bptlab.cepta.operators;
 
 import java.lang.Object;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.functions.IterativeCondition;
-import org.apache.flink.api.common.functions.Context;
-// import org.apache.flink.api.common.functions.FilterFunction;
+//import org.apache.flink.api.common.functions.IterativeCondition;
+//import org.apache.flink.api.common.functions.Context;
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 
-public class RemoveDuplicates<T> {
-    public Pattern<T, ?> duplicates = Pattern.<T>begin("first")
+public class RemoveDuplicates<T extends Object> {
+    /*public Pattern<T, ?> duplicates = Pattern.<T>begin("first")
         .followedBy("second").where(
             new IterativeCondition<T>(){
                 @Override
@@ -18,12 +18,14 @@ public class RemoveDuplicates<T> {
             };
         )
 
-
+*/
     public DataStream<T> removeDuplicates(DataStream<T> stream, int windowSize){
-        DataStream<T> resultStream = inputStream.filter(new FilterFunction<T>() {
+        DataStream<T> resultStream = stream.filter(new FilterFunction<T>() {
             @Override
             public boolean filter(T value) throws Exception {
-              return (!(value.equals(filter)));
+                // TO DO: detect events which already occured and filter them 
+                // TO DO: implement a window size so the filter knows after which time the events can be released
+                return true;
             }
           });
         return resultStream;
