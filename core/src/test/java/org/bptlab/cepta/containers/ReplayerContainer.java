e01ee1574b05ea525d061ac5b8516d34cecf4a32
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-04-17 22:50:16 +0200

    Refactor container configuraition

--- core/src/test/java/org/bptlab/cepta/containers/ReplayerContainer.java
+++ core/src/test/java/org/bptlab/cepta/containers/ReplayerContainer.java
@@ -5,8 +5,21 @@ import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 public class ReplayerContainer extends LocalContainer {
+
+    private String host;
+    private int port;
+
     public ReplayerContainer() {
+        this("localhost", 8080);
+    }
+
+    public ReplayerContainer(String host, int port) {
         super("//auxiliary/producers/replayer");
+        this.host = host;
+        this.port = port;
+        this.withEnv("PORT", Integer.toString(port));
+        this.withEnv("PAUSE", "500"); // 500 milliseconds
+        this.withEnv("REPLAY_MODE", "constant");
         this.waitingFor(
             Wait.forLogMessage(".*Replayer ready.*\\n", 1)
         );
@@ -15,13 +28,16 @@ public class ReplayerContainer extends LocalContainer {
     public static ReplayerContainer forKafka(KafkaContainer kafka) {
         ReplayerContainer replayer = new ReplayerContainer();
         replayer.withNetworkMode("host");
-        replayer.withEnv("REPLAY_MODE", "constant");
-        replayer.withEnv("PAUSE", "500"); // 500 milliseconds
-        replayer.withEnv("PORT", "8080"); // 10 milliseconds
         // Not needed in host nework mode
         // replayer.withEnv("KAFKA_BROKERS", kafka.getBootstrapServers());
         return replayer;
     }
-}
 
-// TODO: Add more microservice containers
+    public String getHost() {
+        return this.host;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+}
