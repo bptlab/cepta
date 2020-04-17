d1a9f150eaf28ff1ec2e9f36adf7f550d8b8e8e7
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-04-17 22:51:56 +0200

    Implement sample internal test using replayer container provider

--- core/src/test/java/org/bptlab/cepta/populate-station-mapping/PopulateStationMappingTests.java
+++ core/src/test/java/org/bptlab/cepta/populate-station-mapping/PopulateStationMappingTests.java
@@ -1,8 +1,9 @@
 package org.bptlab.cepta;
 
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -17,13 +18,15 @@ import org.bptlab.cepta.serialization.GenericBinaryProtoDeserializer;
 import org.bptlab.cepta.config.PostgresConfig;
 import org.bptlab.cepta.operators.LivePlannedCorrelationFunction;
 import org.bptlab.cepta.operators.WeatherLocationCorrelationFunction;
-import org.bptlab.cepta.providers.LiveTrainDataProvider;
-import org.bptlab.cepta.providers.WeatherDataProvider;
+import org.bptlab.cepta.providers.ReplayerProvider;
 import org.bptlab.cepta.containers.ReplayerContainer;
 import org.bptlab.cepta.containers.KafkaContainer;
 import org.testcontainers.Testcontainers;
 import org.bptlab.cepta.models.constants.topic.TopicOuterClass.Topic;
-import org.bptlab.cepta.models.grpc.replayer.ReplayerGrpc;
+import org.bptlab.cepta.models.grpc.replayer.ReplayerOuterClass.ReplayedEvent;
+import org.bptlab.cepta.models.grpc.replayer.ReplayerOuterClass.QueryOptions;
+import org.bptlab.cepta.models.grpc.replayer.ReplayerOuterClass.SourceQueryOptions;
+import org.bptlab.cepta.models.grpc.replayer.ReplayerOuterClass.ReplayOptions;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -42,67 +45,14 @@ public class PopulateStationMappingTests {
       ReplayerContainer replayer = ReplayerContainer.forKafka(kafka);
       replayer.start();
 
+      // Start the replayer for the test
+      ReplayOptions replayOptions = ReplayOptions.newBuilder().setLimit(100).build();
+      SourceQueryOptions gps = SourceQueryOptions.newBuilder().setSource(Topic.GPS_TRIP_UPDATE_DATA).setOptions(replayOptions).build();
+      QueryOptions queryOptions = QueryOptions.newBuilder().addAllSources(Arrays.asList(gps)).build();
+      Iterator<ReplayedEvent> eventStream = new ReplayerProvider(replayer).query(queryOptions);
+
       final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-      FlinkKafkaConsumer011<PlannedTrainData> plannedTrainDataConsumer =
-        new FlinkKafkaConsumer011<>(
-          Topic.PLANNED_TRAIN_DATA.getValueDescriptor().getName(),
-            new GenericBinaryProtoDeserializer<PlannedTrainData>(PlannedTrainData.class),
-            kafka.getConfig().withClientId("PlannedTrainDataMainConsumer").getProperties());
-
-      DataStream<PlannedTrainData> inputStream = env.addSource(plannedTrainDataConsumer);
-      Iterator<PlannedTrainData> inputIterator = DataStreamUtils.collect(inputStream);
-
-      ArrayList<PlannedTrainData> received = new ArrayList<>();
-
-      if (this.debug) {
-        while (true) {
-          TimeUnit.SECONDS.sleep(2);
-          final String logs = replayer.getLogs();
-          System.out.println(logs);
-        }
-      }
-
-      // DataStreamUtils.collect(expectedStream).forEachRemaining(expected::add);
-      while (inputIterator.hasNext() && received.size() < 1000) {
-        received.add(inputIterator.next());
-        System.out.println(received.get(received.size() - 1));
-      }
-
-      /*
-      FlinkKafkaConsumer011<Person> personsKafkaConsumer = new FlinkKafkaConsumer011<Person>(
-            topic, deserializer, getProps(kafka));
-        personsKafkaConsumer.setStartFromEarliest();
-        DataStream<Person> inputStream = expectedStream.getExecutionEnvironment().addSource(personsKafkaConsumer);
-        Iterator<Person> inputIterator = DataStreamUtils.collect(inputStream);
-        */
-
-      /*
-      String address = postgres.getContainerIpAddress();
-      Integer port = postgres.getFirstMappedPort();
-      PostgresConfig postgresConfig = new PostgresConfig().withHost(address).withPort(port).withPassword(postgres.getPassword()).withUser(postgres.getUsername());
-      */
-
-      /*
-      DataStream<LiveTrainData> liveStream = LiveTrainDataProvider.unmatchingLiveTrainDatas();
-      DataStream<Tuple2<LiveTrainData, PlannedTrainData>> correlatedTrainStream = AsyncDataStream
-          .unorderedWait(liveStream, new LivePlannedCorrelationFunction(postgresConfig),
-              100000, TimeUnit.MILLISECONDS, 1);
-
-      Iterator<Tuple2<LiveTrainData, PlannedTrainData>> iterator = DataStreamUtils.collect(correlatedTrainStream);
-      ArrayList<Tuple2<Long, Long>> correlatedIds = new ArrayList<>();
-      while(iterator.hasNext()){
-        Tuple2<LiveTrainData, PlannedTrainData> tuple = iterator.next();
-        if (tuple.f1 == null){
-          correlatedIds.add(new Tuple2<>(tuple.f0.getTrainSectionId(), null));
-        } else{
-          correlatedIds.add(new Tuple2<>(tuple.f0.getTrainSectionId(), tuple.f1.getTrainSectionId()));
-        }
-      }
-      Assert.assertTrue(correlatedIds.contains(new Tuple2<>(11111111L, null)));
-      Assert.assertTrue(correlatedIds.contains(new Tuple2<>(22222222L, null)));
-    }
-      //Assert.assertTrue(true);
-      */
+      eventStream.forEachRemaining(e -> System.out.println(e));
   }
 }
