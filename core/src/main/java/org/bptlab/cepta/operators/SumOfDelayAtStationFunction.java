e524c39a244eacbaa435e3aaf4c10c461fd3fb99
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2020-04-20 12:22:14 +0200

    initial commit with basic plan how to implement Sum of delay at station

---
+++ core/src/main/java/org/bptlab/cepta/operators/SumOfDelayAtStationFunction.java
@@ -0,0 +1,22 @@
+package org.bptlab.cepta.operators;
+
+import java.lang.Object;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+
+public class SumOfDelayAtStationFunction {
+
+    // get Stream of DelayNotification
+
+    // have a Window of X Time to sum up
+    // key after station id
+
+    // sum up delays at each station
+
+    // when window triggers after time
+
+    // process and create Stream of DelayAtStation events
+
+
+}
