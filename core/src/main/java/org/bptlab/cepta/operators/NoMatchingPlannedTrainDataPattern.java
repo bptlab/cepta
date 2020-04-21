cc2ee82cb0b89b84b26ec9cfb54f28d8f8ea0880
Gritta Weisheit <gritta.weisheit@giz.berlin>
2020-04-21 15:05:38 +0200

    add pattern and event for no matching planned train data

---
+++ core/src/main/java/org/bptlab/cepta/operators/NoMatchingPlannedTrainDataPattern.java
@@ -0,0 +1,54 @@
+package org.bptlab.cepta.operators;
+
+import java.util.Map;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.datastream.DataStreamUtils;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.cep.PatternStream;
+import org.apache.flink.cep.CEP;
+import org.apache.flink.cep.pattern.*;
+import org.apache.flink.cep.pattern.conditions.*;
+import org.apache.flink.cep.functions.PatternProcessFunction;
+import org.apache.flink.cep.functions.PatternProcessFunction.Context;
+import org.apache.flink.util.Collector;
+import org.bptlab.cepta.models.events.train.LiveTrainDataProtos.LiveTrainData;
+import org.bptlab.cepta.models.events.train.PlannedTrainDataProtos.PlannedTrainData;
+import org.bptlab.cepta.models.events.correlatedEvents.StaysInStationEventProtos.StaysInStationEvent;
+import org.bptlab.cepta.models.events.correlatedEvents.StaysInStationEventProtos.NoMatchingPlannedTrainDataEvent;
+
+import java.util.*;
+
+public class NoMatchingPlannedTrainDataPattern{
+    /**
+     * this "pattern" recognizes, when an arriving LiveTrainData 
+     * does not have a corresponding PlannedTrainData
+     * 
+     * it only works on a DataStream with already correlated live and planned data
+     */
+
+    public static final Pattern<Tuple2<LiveTrainData, PlannedTrainData>, ?> noPlanned = Pattern.<Tuple2<LiveTrainData, PlannedTrainData>>begin("start")
+        .where(new IterativeCondition<Tuple2<LiveTrainData, PlannedTrainData>>(){
+            @Override
+            public boolean filter(Tuple2<LiveTrainData, PlannedTrainData> event){
+                if (event.f0.status <= 3 && event.f1 == null){
+                    return true;
+                }
+            }
+        });
+    
+    public static PatternProcessFunction<Tuple2<LiveTrainData, PlannedTrainData>, NoMatchingPlannedTrainDataEvent> patternProcessFunction(){
+        return new PatternProcessFunction<Tuple2<LiveTrainData, PlannedTrainData>, NoMatchingPlannedTrainDataEvent>{
+            @Override
+            public void processMatch(Map<String, List<Tuple2<LiveTrainData, PlannedTrainData>>> match, Context ctx, Collector<NoMatchingPlannedTrainDataEvent> out) throws Exception{
+                LiveTrainData liveEvent = match.get("start").get(0);
+                out.collect(NoMatchingPlannedTrainDataEvent.NewBuilder()
+                    .setTrainSectionId(liveEvent.getTrainSectionId())
+                    .setStationId(liveEvent.getStationId())
+                    .setTrainId(liveEvent.getTrainId())
+                    .setEventTime(liveEvent.getEventTime())
+                    .build());
+            }
+        }
+    }
+}
