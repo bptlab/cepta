b7141b9d38a52918507cd7dbc68290fe23981614
Vincent Opitz <vincent.opitz@student.hpi.de>
2020-05-04 13:30:12 +0200

    add comment to collectStreamToVector

--- core/src/test/java/org/bptlab/cepta/utils/Streams.java
+++ core/src/test/java/org/bptlab/cepta/utils/Streams.java
@@ -6,6 +6,7 @@ import org.apache.flink.streaming.api.datastream.DataStreamUtils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Vector;
 
 public class Streams {
 
@@ -25,4 +26,23 @@ public class Streams {
         }
         return outputCollection;
     }
+
+    /**
+     * This method creates a new Vector of Type T which contains all
+     * elements of the inputStream.
+     * @param inputStream of Type T
+     * @param <T>
+     * @return a Vector with all objects in inputStream
+     * @throws IOException
+     */
+    public static <T> Vector<T> collectStreamToVector(DataStream<T> inputStream) throws IOException {
+        //Apparently, the difference between an ArrayList and a Vector is that the Vector is thread-safe but slower.
+        Vector<T> outputCollection = new Vector<>();
+        Iterator<T> streamIterator = DataStreamUtils.collect(inputStream);
+        while (streamIterator.hasNext()) {
+            outputCollection.add(streamIterator.next());
+        }
+        return outputCollection;
+    }
+
 }
