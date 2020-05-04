449de327af068a9d968b637fdbe1504a7af233d0
Vincent Opitz <vincent.opitz@student.hpi.de>
2020-05-04 13:16:00 +0200

    add comment to collectStreamToArrayList

--- core/src/test/java/org/bptlab/cepta/utils/Streams.java
+++ core/src/test/java/org/bptlab/cepta/utils/Streams.java
@@ -9,6 +9,14 @@ import java.util.Iterator;
 
 public class Streams {
 
+    /**
+     * This method creates a new ArrayList of Type T which contains all
+     * elements of the inputStream.
+     * @param inputStream of Type T
+     * @param <T>
+     * @return an ArrayList with all objects in inputStream
+     * @throws IOException
+     */
     public static <T> ArrayList<T> collectStreamToArrayList(DataStream<T> inputStream) throws IOException {
         ArrayList<T> outputCollection = new ArrayList<>();
         Iterator<T> streamIterator = DataStreamUtils.collect(inputStream);
