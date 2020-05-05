46ddec3b27c5323672a89c47b0b855d7813bce15
Christopher <christopher.schulz@student.hpi.de>
2020-05-05 15:36:21 +0200

    [WIP] Adds Skeleton for Mongo Importer

--- core/src/main/java/org/bptlab/cepta/operators/DataToMongoDB.java
+++ core/src/main/java/org/bptlab/cepta/operators/DataToMongoDB.java
@@ -4,9 +4,20 @@ import com.google.protobuf.Message;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.util.Collector;
+import org.bptlab.cepta.config.MongoConfig;
 
-public class DataToMongoDB<T extends Message> implements FlatMapFunction<T> {
+public class DataToMongoDB<T extends Message> implements FlatMapFunction<T,T> {
 
     private String collection_name;
-    private MongoConfig
+    private MongoConfig mongoConfig = new MongoConfig();
+
+    public DataToMongoDB(String collection_name, MongoConfig mongoConfig){
+        this.collection_name = collection_name;
+        this.mongoConfig = mongoConfig;
+    }
+
+    @Override
+    public void flatMap(T dataset, Collector<T> collector) throws Exception {
+
+    }
 }
