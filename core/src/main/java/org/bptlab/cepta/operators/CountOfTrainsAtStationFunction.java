87b42406f932129b705b0949a50e76dc2523868a
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2020-05-06 11:13:11 +0200

    implement count of trains at station operator

---
+++ core/src/main/java/org/bptlab/cepta/operators/CountOfTrainsAtStationFunction.java
@@ -0,0 +1,50 @@
+package org.bptlab.cepta.operators;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.bptlab.cepta.utils.triggers.CustomCountTrigger;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.bptlab.cepta.models.events.train.LiveTrainDataOuterClass.LiveTrainData;
+
+import org.apache.flink.util.Collector;
+
+import org.apache.flink.api.java.functions.KeySelector;
+
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+
+public class CountOfTrainsAtStationFunction {
+
+    public DataStream<Tuple2<Long, Integer>> CountOfTrainsAtStation(DataStream<LiveTrainData> inputStream) {
+        DataStream<Tuple2<Long, Integer>> resultStream = inputStream
+        .keyBy(
+            new KeySelector<LiveTrainData, Long>(){
+                public Long getKey(LiveTrainData event){
+                    return event.getStationId();
+                }
+            }
+        )
+        .window(SlidingEventTimeWindows.of(Time.hours(1), Time.seconds(900)))
+        .process(
+            CountOfTrainsAtStationProcessFunction()
+        );
+        return resultStream;
+    };
+
+    public ProcessWindowFunction<LiveTrainData, Tuple2<Long, Integer>, Long, TimeWindow> CountOfTrainsAtStationProcessFunction() {
+        return new ProcessWindowFunction<LiveTrainData, Tuple2<Long, Integer>, Long, TimeWindow>() {
+            @Override
+            public void process(Long key, Context context, Iterable<LiveTrainData> input, Collector<Tuple2<Long, Integer>> out) throws Exception {
+                int counter = 0;
+                for (Object i : input) {
+                    counter++;
+                }
+                out.collect(new Tuple2<Long, Integer>(key, counter) );
+            }
+        };
+    };
+
+}
