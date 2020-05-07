4ac8f9284e68fb62ca6f6236829cc08d6a24e11e
Vincent Opitz <vincent.opitz@student.hpi.de>
2020-05-07 11:33:42 +0200

    [FEAT] test wether each station is counted independently

--- core/src/test/java/org/bptlab/cepta/count-trains-in-station-tests/CountTrainsInStationTests.java
+++ core/src/test/java/org/bptlab/cepta/count-trains-in-station-tests/CountTrainsInStationTests.java
@@ -37,14 +37,12 @@ public class CountTrainsInStationTests {
                 LiveTrainDataProvider.getDefaultLiveTrainDataEvent().toBuilder()
                         .setStationId(commonStation)
                         .setTrainId(2)
-                        .setIngestionTime(Timestamps.parse("1972-01-01T10:00:21.022-05:00")).build()
-                ;
+                        .setIngestionTime(Timestamps.parse("1972-01-01T10:00:21.022-05:00")).build();
         LiveTrainDataOuterClass.LiveTrainData end =
                 LiveTrainDataProvider.getDefaultLiveTrainDataEvent().toBuilder()
                         .setStationId(commonStation)
                         .setTrainId(3)
-                        .setIngestionTime(Timestamps.parse("2042-01-01T10:00:31.023-05:00")).build()
-                ;
+                        .setIngestionTime(Timestamps.parse("2042-01-01T10:00:31.023-05:00")).build();
 
         DataStream<LiveTrainDataOuterClass.LiveTrainData> liveTrainStream =
                 env.fromElements(first, second, end)
@@ -55,7 +53,6 @@ public class CountTrainsInStationTests {
                         return liveTrainData.getIngestionTime().getSeconds();
                     }
                 });
-
 
         DataStream<Tuple2<Long, Integer>> countOfStationStream = CountOfTrainsAtStationFunction.countOfTrainsAtStation(liveTrainStream);
         ArrayList<Tuple2<Long,Integer>> resultCollection = StreamUtils.collectStreamToArrayList(countOfStationStream);
@@ -105,6 +102,57 @@ public class CountTrainsInStationTests {
                 Assert.assertEquals(Long.valueOf(1L), in.f0);
                 Assert.assertEquals(Integer.valueOf(2), in.f1);
         }
+    }
+
+    @Test
+    public void TestCountEachStationIndependently() throws IOException, ParseException {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        int commonStation = 1;
+        LiveTrainDataOuterClass.LiveTrainData first =
+                LiveTrainDataProvider.getDefaultLiveTrainDataEvent().toBuilder()
+                        .setStationId(commonStation)
+                        .setTrainId(1)
+                        .setIngestionTime(Timestamps.parse("1972-01-01T10:00:20.021-05:00")).build();
+        LiveTrainDataOuterClass.LiveTrainData second =
+                LiveTrainDataProvider.getDefaultLiveTrainDataEvent().toBuilder()
+                        .setStationId(commonStation + 1)
+                        .setTrainId(2)
+                        .setIngestionTime(Timestamps.parse("1972-01-01T10:00:21.022-05:00")).build()
+                ;
+        LiveTrainDataOuterClass.LiveTrainData end =
+                LiveTrainDataProvider.getDefaultLiveTrainDataEvent().toBuilder()
+                        .setStationId(commonStation)
+                        .setTrainId(3)
+                        .setIngestionTime(Timestamps.parse("2042-01-01T10:00:31.023-05:00")).build()
+                ;
+
+        DataStream<LiveTrainDataOuterClass.LiveTrainData> liveTrainStream =
+                env.fromElements(first, second, end)
+                        .assignTimestampsAndWatermarks(
+                                new AscendingTimestampExtractor<LiveTrainDataOuterClass.LiveTrainData>() {
+                                    @Override
+                                    public long extractAscendingTimestamp(LiveTrainDataOuterClass.LiveTrainData liveTrainData) {
+                                        return liveTrainData.getIngestionTime().getSeconds();
+                                    }
+                                });
+
+
+        DataStream<Tuple2<Long, Integer>> countOfStationStream = CountOfTrainsAtStationFunction.countOfTrainsAtStation(liveTrainStream);
+        ArrayList<Tuple2<Long,Integer>> resultCollection = StreamUtils.collectStreamToArrayList(countOfStationStream);
+
+        boolean onlyOneTrainPerStation = true;
+        for (Tuple2<Long,Integer> in : resultCollection) {
+            /*
+            check that each window only had one train per station
+            with .f1 being the count of the window
+           */
+            if (in.f1 != 1) {
+                onlyOneTrainPerStation = false;
+            }
+        }
+
+        Assert.assertTrue(onlyOneTrainPerStation);
 
     }
 }
