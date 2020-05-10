e101e3164173803882092d40acf78399a02ced25
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-05-10 18:08:52 +0200

    Use MD5 for hashing cepta ids

--- core/src/main/java/org/bptlab/cepta/utils/IDGenerator.java
+++ core/src/main/java/org/bptlab/cepta/utils/IDGenerator.java
@@ -1,9 +1,20 @@
 package org.bptlab.cepta.utils;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import java.util.UUID;
 
 public class IDGenerator {
+
+    public static <T extends String>String hashed(T input) {
+        return DigestUtils.md5Hex(input).toUpperCase();
+    }
+
+    public static <T extends Number>String hashed(T input) {
+        return DigestUtils.md5Hex(String.valueOf(input)).toUpperCase();
+    }
+
     public static String newCeptaID() {
-        return String.format("CPTA-%s", UUID.randomUUID().toString());
+        return String.format("CPTA-%s", hashed(UUID.randomUUID().toString()));
     }
 }
--- core/src/test/java/org/bptlab/cepta/utils/IDGeneratorTest.java
+++ core/src/test/java/org/bptlab/cepta/utils/IDGeneratorTest.java
@@ -1,10 +1,10 @@
 package org.bptlab.cepta.test.utils;
 
+import org.javatuples.Quartet;
 import org.junit.Assert;
 import org.junit.Test;
 import org.bptlab.cepta.utils.IDGenerator;
-import java.io.IOException;
-import java.lang.reflect.Array;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -13,11 +13,9 @@ import java.util.stream.Collectors;
 
 public class IDGeneratorTest {
 
-    protected boolean hasDuplicates(List<String> ids)
-    {
+    protected boolean hasDuplicates(List<String> ids) {
         Set<String> s = new HashSet<>();
-        for (String i : ids)
-        {
+        for (String i : ids) {
             if (s.contains(i)) return true;
             s.add(i);
         }
@@ -25,10 +23,36 @@ public class IDGeneratorTest {
     }
 
     @Test
-  public void testGeneratesUniqueIds() {
+    public void testGeneratesUniqueIds() {
         List<String> ids = Arrays.asList(new String[1000]).parallelStream().map(i -> IDGenerator.newCeptaID())
                 .collect(Collectors.toList());
         Assert.assertFalse(hasDuplicates(ids));
-  }
+    }
+
+    @Test
+    public void testHashIsDeterministic() {
+        List<Quartet<Object,Object,String, String>> shouldEqual = Arrays.asList(
+                new Quartet<>("12", 12, IDGenerator.hashed("12"), IDGenerator.hashed(12L)),
+                new Quartet<>(12L, 12, IDGenerator.hashed(12L), IDGenerator.hashed(12)),
+                new Quartet<>("12", "12", IDGenerator.hashed("12"), IDGenerator.hashed("12")),
+                new Quartet<>(12L, "12", IDGenerator.hashed(12L), IDGenerator.hashed("12"))
+        );
+        for (Quartet se: shouldEqual) {
+            if (!se.getValue2().equals(se.getValue3())) {
+                Assert.fail(String.format("Expected hashed(%s) == hashed(%s) but got %s != %s", se.getValue0(), se.getValue1(), se.getValue2(), se.getValue3()));
+            }
+        }
+        List<Quartet<Object,Object,String, String>> shouldDiffer = Arrays.asList(
+                new Quartet<>("12.0", 12L, IDGenerator.hashed("12.0"), IDGenerator.hashed(12L)),
+                new Quartet<>("12A", "12B", IDGenerator.hashed("12A"), IDGenerator.hashed("12B")),
+                new Quartet<>("a", "A", IDGenerator.hashed("a"), IDGenerator.hashed("A")),
+                new Quartet<>(12F, 12L, IDGenerator.hashed(12F), IDGenerator.hashed(12L))
+        );
+        for (Quartet sd: shouldDiffer) {
+            if (sd.getValue2().equals(sd.getValue3())) {
+                Assert.fail(String.format("Expected hashed(%s) != hashed(%s) but got %s == %s", sd.getValue0(), sd.getValue1(), sd.getValue2(), sd.getValue3()));
+            }
+        }
+    }
 
 }
