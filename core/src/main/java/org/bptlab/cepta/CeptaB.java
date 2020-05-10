c919a7140814c1ddf85b0566b5a6c2e828c3683b
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-05-10 18:06:53 +0200

    Draft for generic processing pipeline

--- core/src/main/java/org/bptlab/cepta/CeptaB.java
+++ core/src/main/java/org/bptlab/cepta/CeptaB.java
@@ -20,60 +20,104 @@ package org.bptlab.cepta;
 
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.bptlab.cepta.config.KafkaConfig;
-import org.bptlab.cepta.config.PostgresConfig;
 import org.bptlab.cepta.models.constants.topic.TopicOuterClass.Topic;
 import org.bptlab.cepta.models.events.event.EventOuterClass.Event;
+import org.bptlab.cepta.models.internal.event.Event.NormalizedEvent;
+import org.bptlab.cepta.models.events.station.StationDataOuterClass.StationData;
 import org.bptlab.cepta.models.events.train.LiveTrainDataOuterClass.LiveTrainData;
 import org.bptlab.cepta.models.events.train.PlannedTrainDataOuterClass.PlannedTrainData;
 import org.bptlab.cepta.models.events.weather.WeatherDataOuterClass.WeatherData;
 import org.bptlab.cepta.models.internal.notifications.notification.NotificationOuterClass;
+import org.bptlab.cepta.models.internal.station.StationOuterClass;
+import org.bptlab.cepta.models.internal.transport.TransportOuterClass;
+import org.bptlab.cepta.models.internal.updates.plan.PlanUpdateOuterClass;
+import org.bptlab.cepta.normalization.LiveTrainDataNormalizationFunction;
+import org.bptlab.cepta.normalization.PlannedTrainDataNormalizationFunction;
+import org.bptlab.cepta.normalization.StationDataNormalizationFunction;
+import org.bptlab.cepta.operators.aggregators.AggregateTransportsFunction;
+import org.bptlab.cepta.operators.aggregators.FlinkStateAggregateTransportsFunction;
 import org.bptlab.cepta.serialization.GenericBinaryProtoSerializer;
-import java.util.HashMap;
-import java.util.Optional;
+
+import java.util.*;
+
+/*
+    NOTE: Do not alter this implementation. Instead, extend this class or the Cepta class and replace the class in Main!
+*/
 
 public class CeptaB extends Cepta {
 
-  // Producers
-  private FlinkKafkaProducer011<NotificationOuterClass.Notification> trainDelayNotificationProducer;
+    // Producers
+    protected FlinkKafkaProducer011<NotificationOuterClass.Notification> trainDelayNotificationProducer;
+    protected FlinkKafkaProducer011<PlanUpdateOuterClass.PlanUpdate> transportPlanUpdateProducer;
 
-  protected CeptaB(Cepta.Builder builder) {
-    super(builder);
-  }
+    protected CeptaB(Cepta.Builder builder) {
+        super(builder);
+    }
 
-  public static class Builder extends Cepta.Builder<CeptaA.Builder> {
-    public CeptaB build() { return new CeptaB(this); }
-  }
+    public static class Builder extends Cepta.Builder<CeptaA.Builder> {
+        public CeptaB build() {
+            return new CeptaB(this);
+        }
+    }
 
-  private void setupProducers() {
-    KafkaConfig delaySenderConfig = new KafkaConfig().withClientId("TrainDelayNotificationProducer")
-            .withKeySerializer(Optional.of(LongSerializer::new));
-    this.trainDelayNotificationProducer = new FlinkKafkaProducer011<>(
-            Topic.DELAY_NOTIFICATIONS.getValueDescriptor().getName(),
-            new GenericBinaryProtoSerializer<>(),
-            delaySenderConfig.getProperties());
-    this.trainDelayNotificationProducer.setWriteTimestampToKafka(true);
-  }
+    private void setupProducers() {
+        KafkaConfig trainDelayNotificationProducerConfig = new KafkaConfig().withClientId("TrainDelayNotificationProducer").withKeySerializer(Optional.of(LongSerializer::new));
+        this.trainDelayNotificationProducer = new FlinkKafkaProducer011<>(
+                Topic.DELAY_NOTIFICATIONS.getValueDescriptor().getName(),
+                new GenericBinaryProtoSerializer<>(),
+                trainDelayNotificationProducerConfig.getProperties());
+        this.trainDelayNotificationProducer.setWriteTimestampToKafka(true);
 
-  @Override
-  public void start() throws Exception {
-    logger.info("Starting CEPTA core... [B]");
+        KafkaConfig transportPlanUpdateProducerConfig = new KafkaConfig().withClientId("TrainDelayNotificationProducer").withKeySerializer(Optional.of(LongSerializer::new));
+        this.transportPlanUpdateProducer = new FlinkKafkaProducer011<>(
+                Topic.TRANSPORT_PLAN_UPDATES.getValueDescriptor().getName(),
+                new GenericBinaryProtoSerializer<>(),
+                transportPlanUpdateProducerConfig.getProperties());
+        this.transportPlanUpdateProducer.setWriteTimestampToKafka(true);
+    }
 
-    env.setParallelism(1);
-    this.setupProducers();
+    @Override
+    public void start() throws Exception {
+        logger.info("Starting CEPTA core... [B]");
 
-    DataStream<Event> plannedTrainDataEvents = env.addSource(this.sources.get(Topic.PLANNED_TRAIN_DATA));
-    DataStream<Event> liveTrainDataEvents = env.addSource(this.sources.get(Topic.LIVE_TRAIN_DATA));
-    DataStream<Event> weatherDataEvents = env.addSource(this.sources.get(Topic.WEATHER_DATA));
+        this.setupProducers();
 
-    DataStream<PlannedTrainData> plannedTrainDataStream = plannedTrainDataEvents.map((MapFunction<Event, PlannedTrainData>) Event::getPlannedTrain);
-    DataStream<LiveTrainData> liveTrainDataStream = liveTrainDataEvents.map((MapFunction<Event, LiveTrainData>) Event::getLiveTrain);
-    DataStream<WeatherData> weatherDataStream = weatherDataEvents.map((MapFunction<Event, WeatherData>) Event::getWeather);
+        DataStream<Event> plannedTrainDataEvents = env.addSource(this.sources.get(Topic.PLANNED_TRAIN_DATA));
+        DataStream<Event> stationDataEvents = env.addSource(this.sources.get(Topic.STATION_DATA));
+        DataStream<Event> liveTrainDataEvents = env.addSource(this.sources.get(Topic.LIVE_TRAIN_DATA));
+        DataStream<Event> weatherDataEvents = env.addSource(this.sources.get(Topic.WEATHER_DATA));
 
-    env.execute("CEPTA CORE");
-  }
+        DataStream<NormalizedEvent> normalizedPlanUpdateStream = plannedTrainDataEvents.flatMap(new PlannedTrainDataNormalizationFunction());
+        DataStream<NormalizedEvent> normalizedStationStream = stationDataEvents.flatMap(new StationDataNormalizationFunction());
+        DataStream<NormalizedEvent> normalizedLiveUpdateStream = liveTrainDataEvents.flatMap(new LiveTrainDataNormalizationFunction());
+
+        // TODO:  Aggregate normalized events
+        // TODO:  Choose a good middle ground for window size, so that most connected transports fall into this window and longer transports
+        //        will be fine to asynchronously call out to the query grpc service on a slow extra stream.
+        //        TODO: We need a way to differentiate between a "first transport event" and a "late transport event" that is part of a long running transport
+        //        One idea would be to have unfinished transport metadata kept in memory at the end of a stream that is passed to the next window
+        //        Or immediately dispatch unfinished transports to another stream when the window closes?
+
+        DataStream<TransportOuterClass.Transport> aggregatedStateStream = FlinkStateAggregateTransportsFunction.newBuilder()
+                .setEnv(env)
+                .setPlanUpdates(normalizedPlanUpdateStream)
+                .setLiveUpdates(normalizedLiveUpdateStream)
+                .setGlobalData(normalizedStationStream)
+                .build()
+                .aggregate();
+
+        // TODO: Look for patterns in any normalized data stream or the aggregated data stream
+
+        // TODO: Join aggregated stream with the singular pattern matches and reason about sending delay notifications
+
+        // TODO
+
+        // Send normalized plan data to archival
+        // normalizedPlanUpdateStream.addSink(transportPlanUpdateProducer);
+
+        env.execute("CEPTA CORE");
+    }
 }
