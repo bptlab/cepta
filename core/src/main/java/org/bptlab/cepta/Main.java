2a721f1c2dc2b93643b816af519336229ae53407
Vincent Opitz <vincent.opitz@student.hpi.de>
2020-05-26 13:12:06 +0200

    remove wrong lines from merge and comment out duplicate code

--- core/src/main/java/org/bptlab/cepta/Main.java
+++ core/src/main/java/org/bptlab/cepta/Main.java
@@ -37,11 +37,9 @@ import org.bptlab.cepta.config.KafkaConfig;
 import org.bptlab.cepta.config.MongoConfig;
 import org.bptlab.cepta.config.PostgresConfig;
 import org.bptlab.cepta.models.constants.topic.TopicOuterClass.Topic;
-<<<<<<<<< Temporary merge branch 1
 import org.bptlab.cepta.models.internal.notifications.notification.NotificationOuterClass;
 import org.bptlab.cepta.operators.*;
 import org.bptlab.cepta.models.internal.types.ids.Ids;
-=========
 import org.bptlab.cepta.models.events.correlatedEvents.CountOfTrainsAtStationEventOuterClass.*;
 import org.bptlab.cepta.models.events.correlatedEvents.NoMatchingPlannedTrainDataEventOuterClass.NoMatchingPlannedTrainDataEvent;
 import org.bptlab.cepta.models.events.info.LocationDataOuterClass.LocationData;
@@ -269,17 +267,16 @@ public class Main implements Callable<Integer> {
      * Begin - CountOfTrainsAtStation
      * ------------------------*/
 
-       DataStream<NotificationOuterClass.DelayNotification> delayShiftNotifications = AsyncDataStream
+       DataStream<NotificationOuterClass.Notification> delayShiftNotifications = AsyncDataStream
           .unorderedWait(liveTrainDataStream, new DelayShiftFunction(postgresConfig),
             100000, TimeUnit.MILLISECONDS, 1);
 
 //    countOfTrainsAtStationDataStream.print();
 
->>>>>>>>> Temporary merge branch 2
-    DataStream<Tuple2<LiveTrainData, PlannedTrainData>> matchedLivePlannedStream =
-        AsyncDataStream
-            .unorderedWait(liveTrainDataStream, new LivePlannedCorrelationFunction(postgresConfig),
-                100000, TimeUnit.MILLISECONDS, 1);
+//    DataStream<Tuple2<LiveTrainData, PlannedTrainData>> matchedLivePlannedStream =
+//        AsyncDataStream
+//            .unorderedWait(liveTrainDataStream, new LivePlannedCorrelationFunction(postgresConfig),
+//                100000, TimeUnit.MILLISECONDS, 1);
 
     // LivePlannedCorrelationFunction Mongo
     DataStream<Tuple2<LiveTrainData, PlannedTrainData>> matchedLivePlannedStream = AsyncDataStream
