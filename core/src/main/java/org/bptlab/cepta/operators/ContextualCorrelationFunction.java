0afc6782603ee06575dffdaa54504bf9b71921dc
Vincent <vincent.opitz@student.hpi.de>
2020-06-03 19:51:38 +0200

    fix typo

--- core/src/main/java/org/bptlab/cepta/operators/ContextualCorrelationFunction.java
+++ core/src/main/java/org/bptlab/cepta/operators/ContextualCorrelationFunction.java
@@ -2,17 +2,23 @@ package org.bptlab.cepta.operators;
 
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.util.Collector;
+import org.bptlab.cepta.config.MongoConfig;
 import org.bptlab.cepta.models.events.train.LiveTrainDataOuterClass;
-import org.bptlab.cepta.models.internal.correlatable_event.CorrelateableEvent;
+import org.bptlab.cepta.models.internal.correlatable_event.CorrelateableEventOuterClass;
 
-public class ContextualCorrelationFunction extends RichFlatMapFunction<LiveTrainDataOuterClass.LiveTrainData, CorrelateableEvent.CorrelatableEvent> {
+public class ContextualCorrelationFunction extends RichFlatMapFunction<LiveTrainDataOuterClass.LiveTrainData, CorrelateableEventOuterClass.CorrelateableEvent {
 
-    public void open(org.apache.flink.configuration.Configuration parameters) throws Exception {
-        super.open(parameters);
+    private StationToCoordinateMap stationToCoordinateMap;
+
+    ContextualCorrelationFunction(String datebaseName, String tableName, MongoConfig mongoConfig) {
+        this.stationToCoordinateMap = new StationToCoordinateMap(datebaseName, tableName, mongoConfig);
     }
 
+    public StationToCoordinateMap getMapping(){ return stationToCoordinateMap;}
+
     @Override
-    public void flatMap(LiveTrainDataOuterClass.LiveTrainData liveTrainData, Collector<CorrelateableEvent.CorrelatableEvent> collector) throws Exception {
+    public void flatMap(LiveTrainDataOuterClass.LiveTrainData liveTrainData, Collector<CorrelateableEventOuterClass.CorrelateableEvent> collector) throws Exception {
+
 
     }
 }
---
+++ core/src/main/java/org/bptlab/cepta/operators/StationToCoordinateMap.java
@@ -0,0 +1,64 @@
+package org.bptlab.cepta.operators;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoCollection;
+import com.mongodb.reactivestreams.client.MongoDatabase;
+import org.bptlab.cepta.config.MongoConfig;
+import org.bptlab.cepta.models.internal.types.coordinate.CoordinateOuterClass;
+import org.bptlab.cepta.utils.database.mongohelper.SubscriberHelpers;
+import org.bson.Document;
+
+import java.util.Hashtable;
+import java.util.List;
+
+public class StationToCoordinateMap extends Hashtable<Long, CoordinateOuterClass.Coordinate> {
+    private MongoConfig mongoConfig = new MongoConfig();
+    private transient MongoClient mongoClient;
+    private String tableName;
+    private String databaseName;
+
+    public StationToCoordinateMap(MongoConfig mongoConfig){
+        this.mongoConfig = mongoConfig;
+        this.tableName = "eletastations";
+        this.databaseName = "replay";
+        readInStationData();
+    }
+
+    public StationToCoordinateMap(String tableName, MongoConfig mongoConfig){
+        this.mongoConfig = mongoConfig;
+        this.tableName = tableName;
+        this.databaseName = "replay";
+        readInStationData();
+    }
+
+    public StationToCoordinateMap(String datebaseName, String tableName, MongoConfig mongoConfig){
+        this.mongoConfig = mongoConfig;
+        this.tableName = tableName;
+        this.databaseName = datebaseName;
+        readInStationData();
+    }
+
+    private boolean readInStationData(){
+        MongoDatabase database = mongoClient.getDatabase(databaseName);
+        MongoCollection<Document> stationsCollection = database.getCollection(tableName);
+
+        SubscriberHelpers.OperationSubscriber<Document> findMultipleSubscriber = new SubscriberHelpers.OperationSubscriber<>();
+        stationsCollection.find().subscribe(findMultipleSubscriber);
+
+        List<Document> allStations = findMultipleSubscriber.get();
+
+        for (Document station: allStations){
+            CoordinateOuterClass.Coordinate coordinate =
+                    CoordinateOuterClass.Coordinate.newBuilder()
+                            .setLongitude((double) station.get("longitude"))
+                            .setLatitude((double) station.get("latitude"))
+                            .build();
+            Long key = (Long) station.get("stationId");
+            this.put(key, coordinate);
+        }
+        return true;
+    }
+
+
+
+}
