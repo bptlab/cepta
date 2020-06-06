7511ccd9ce8ce8bc06e5a1fbe364fc7caa49c85b
Vincent <vincent.opitz@student.hpi.de>
2020-06-07 00:03:04 +0200

    add WIP function for retrieving a whole trainrun

--- core/src/test/java/org/bptlab/cepta/contextual-correlation-tests/ContextualCorrelationTests.java
+++ core/src/test/java/org/bptlab/cepta/contextual-correlation-tests/ContextualCorrelationTests.java
@@ -1,18 +1,29 @@
 package org.bptlab.cepta;
 
 import com.google.protobuf.util.Timestamps;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoCollection;
+import com.mongodb.reactivestreams.client.MongoDatabase;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.bptlab.cepta.config.MongoConfig;
 import org.bptlab.cepta.models.events.train.LiveTrainDataOuterClass.*;
 import org.bptlab.cepta.models.internal.correlateable_event.CorrelateableEventOuterClass.*;
 import org.bptlab.cepta.operators.ContextualCorrelationFunction;
+import org.bptlab.cepta.utils.database.Mongo;
+import org.bptlab.cepta.utils.database.mongohelper.SubscriberHelpers;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
 import org.bptlab.cepta.utils.functions.StreamUtils;
+import org.bson.Document;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.swing.plaf.basic.BasicListUI;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.Vector;
 
 public class ContextualCorrelationTests{
@@ -65,6 +76,29 @@ public class ContextualCorrelationTests{
 
     }
 
+    private MongoClient getMongoClient(){
+        return Mongo.getMongoClient(
+                new MongoConfig()
+                        .withHost("localhost")
+                        .withPort(27017)
+                        .withPassword("example")
+                        .withUser("root")
+                        .withName("mongodb")
+        );
+    }
+
+    private Vector<LiveTrainData> getLiveTrainsOfEuroRailRunId(int euroRailRunId){
+        MongoClient mongoClient = this.getMongoClient();
+        SubscriberHelpers.OperationSubscriber<Document> findMultipleSubscriber = new SubscriberHelpers.OperationSubscriber<>();
+
+        MongoDatabase database = mongoClient.getDatabase("replay");
+        MongoCollection<Document> collection = database.getCollection("trainsectiondata");
+        collection.aggregate(Arrays.asList(
+                Aggregates.match(Filters.eq("euroRailRunId", euroRailRunId)),
+                Aggregates.project(Document.parse("{euroRailRunId:1}"))
+            )).subscribe(findMultipleSubscriber);
+        return null;
+    }
 
 
 }
