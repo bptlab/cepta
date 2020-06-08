de47cb73acc5427dac2e23f029acdd9c25190898
Vincent <vincent.opitz@student.hpi.de>
2020-06-08 15:35:26 +0200

    Add basic test for one train run

--- core/src/test/java/org/bptlab/cepta/contextual-correlation-tests/ContextualCorrelationTests.java
+++ core/src/test/java/org/bptlab/cepta/contextual-correlation-tests/ContextualCorrelationTests.java
@@ -5,28 +5,31 @@ import com.mongodb.client.model.Aggregates;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoCollection;
 import com.mongodb.reactivestreams.client.MongoDatabase;
+
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 import org.bptlab.cepta.config.MongoConfig;
 import org.bptlab.cepta.models.events.train.LiveTrainDataOuterClass.*;
 import org.bptlab.cepta.models.internal.correlateable_event.CorrelateableEventOuterClass.*;
+import org.bptlab.cepta.models.internal.types.ids.Ids;
 import org.bptlab.cepta.operators.ContextualCorrelationFunction;
 import org.bptlab.cepta.utils.database.Mongo;
 import org.bptlab.cepta.utils.database.mongohelper.SubscriberHelpers;
-import com.mongodb.client.model.Aggregates;
-import com.mongodb.client.model.Filters;
 import org.bptlab.cepta.utils.functions.StreamUtils;
+
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 import org.junit.Assert;
 import org.junit.Test;
 
-import javax.swing.plaf.basic.BasicListUI;
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.Arrays;
-import java.util.Vector;
+import java.util.*;
 
 public class ContextualCorrelationTests{
+
+    private MongoClient mongoClient = this.getMongoClient();
 
     private StreamExecutionEnvironment setupEnv(){
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -35,25 +38,8 @@ public class ContextualCorrelationTests{
     }
 
     @Test
-    public void TestFunctionCallThrowsNoErrors() throws ParseException, IOException {
+    public void TestOneGlobalTrainRunGetsCorrelatedTogether() throws ParseException, IOException {
         StreamExecutionEnvironment env = setupEnv();
-
-        LiveTrainData sampleTrain1 =
-                LiveTrainData
-                        .newBuilder()
-                        .setTrainId(1)
-                        .setStationId(4019302)
-                        .setEventTime(Timestamps.parse("2019-04-28T09:04:00.000Z"))
-                        .setTrainSectionId(42)
-                        .build();
-        LiveTrainData sampleTrain2 =
-                LiveTrainData
-                        .newBuilder()
-                        .setTrainId(2)
-                        .setStationId(4012434)
-                        .setEventTime(Timestamps.parse("2019-04-28T09:05:00.000Z"))
-                        .setTrainSectionId(80085)
-                        .build();
 
         MongoConfig mongoConfig = new MongoConfig()
                 .withHost("localhost")
@@ -62,18 +48,22 @@ public class ContextualCorrelationTests{
                 .withUser("root")
                 .withName("mongodb");
 
-        DataStream<LiveTrainData> liveTrainDataDataStream = env.fromElements(sampleTrain1, sampleTrain2).assignTimestampsAndWatermarks(StreamUtils.eventTimeExtractor());
+        DataStream<LiveTrainData> liveTrainDataDataStream = env
+                .fromCollection(getLiveTrainsOfEuroRailRunId(40510063))
+                .assignTimestampsAndWatermarks(StreamUtils.eventTimeExtractor());
 
         DataStream<CorrelateableEvent> sampleStream = liveTrainDataDataStream
-                        .flatMap(new ContextualCorrelationFunction("replay", "eletastations",mongoConfig));
+                        .flatMap(new ContextualCorrelationFunction("replay", "eletastations", mongoConfig));
 
 
         Vector<CorrelateableEvent> sampleStreamElements = StreamUtils.collectStreamToVector(sampleStream);
         System.out.println(sampleStreamElements);
 
-        sampleStream.print();
-        Assert.assertTrue(true);
 
+        Vector<Ids.CeptaTransportID> allIds = new Vector<>();
+        sampleStreamElements.forEach(event -> allIds.add(event.getCeptaId()));
+        HashSet<Ids.CeptaTransportID> distinctIds = new HashSet<>(allIds);
+        Assert.assertEquals("One Trainrun should be correlated to one ID",1,distinctIds.size());
     }
 
     private MongoClient getMongoClient(){
@@ -87,18 +77,41 @@ public class ContextualCorrelationTests{
         );
     }
 
+    private void addLiveTrains(Long trainSectionId, Vector<LiveTrainData> outputVector){
+        SubscriberHelpers.OperationSubscriber<Document> subscriber = new SubscriberHelpers.OperationSubscriber<Document>();
+        this.mongoClient
+                .getDatabase("replay")
+                .getCollection("livetraindata")
+                .find(Filters.eq("trainSectionId", trainSectionId))
+                .subscribe(subscriber);
+        List<Document> allLiveTrainEvents = subscriber.get();
+        allLiveTrainEvents.forEach(document -> outputVector.add(Mongo.documentToLiveTrainData(document)));
+    }
+
     private Vector<LiveTrainData> getLiveTrainsOfEuroRailRunId(int euroRailRunId){
-        MongoClient mongoClient = this.getMongoClient();
         SubscriberHelpers.OperationSubscriber<Document> findMultipleSubscriber = new SubscriberHelpers.OperationSubscriber<>();
 
         MongoDatabase database = mongoClient.getDatabase("replay");
         MongoCollection<Document> collection = database.getCollection("trainsectiondata");
         collection.aggregate(Arrays.asList(
                 Aggregates.match(Filters.eq("euroRailRunId", euroRailRunId)),
-                Aggregates.project(Document.parse("{euroRailRunId:1}"))
+                Aggregates.project(Document.parse("{trainSectionId:1}"))
             )).subscribe(findMultipleSubscriber);
-        return null;
+
+        List<Document> trainSectionIdsDocuments = findMultipleSubscriber.get();
+        Vector<Long> trainSectionIds = new Vector<>();
+        trainSectionIdsDocuments.forEach(document -> trainSectionIds.add(document.getLong("trainSectionId")));
+
+        //since we could have multiple entries for each trainRun, we make these values distinct
+        HashSet<Long> uniqueTrainSectionIds = new HashSet<>(trainSectionIds);
+
+        Vector<LiveTrainData> allLiveTrainEvents = new Vector<>();
+        //now we query for each liveTrain event within these sections
+        uniqueTrainSectionIds.forEach(trainSectionId -> addLiveTrains(trainSectionId, allLiveTrainEvents));
+
+        return allLiveTrainEvents;
     }
+
 
 
 }
