30e56ea3eda97d02d132a2ead28d8ef6e683d3ed
Vincent <vincent.opitz@student.hpi.de>
2020-06-10 20:24:43 +0200

    ref indentation

--- core/src/main/java/org/bptlab/cepta/operators/ContextualCorrelationFunction.java
+++ core/src/main/java/org/bptlab/cepta/operators/ContextualCorrelationFunction.java
@@ -57,10 +57,10 @@ public class ContextualCorrelationFunction extends RichFlatMapFunction<LiveTrain
 
         Vector<Pair<Entry<CorrelateableEvent, Geometry>, Double>> closeEvents = new Vector<>();
         currentEvents.search(pointLocation, 50, (a, b) -> {
-            Position positionA = Position.create(a.mbr().y1(),a.mbr().x1());
-            Position positionB = Position.create(b.mbr().y1(),b.mbr().x1());
-            return positionA.getDistanceToKm(positionB);
-        })
+                    Position positionA = Position.create(a.mbr().y1(),a.mbr().x1());
+                    Position positionB = Position.create(b.mbr().y1(),b.mbr().x1());
+                    return positionA.getDistanceToKm(positionB);
+                })
                 //filter out events that are on the same point as the variable
                 .filter(entry -> { return entry.geometry().distance(pointLocation) != 0;})
                 // map each incoming entry to a pair of the entry and its distance to the uncorrelated event
