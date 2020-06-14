4e53c4da51c14e23a2161a37c770fa367a170106
Gritta Weisheit <gritta.weisheit@giz.berlin>
2020-06-14 16:33:13 +0200

    easier startup

--- auxiliary/producers/replayer/replayers.go
+++ auxiliary/producers/replayer/replayers.go
@@ -100,7 +100,7 @@ func (s *ReplayerServer) Setup(ctx context.Context) error {
 	}
 
 	s.LiveTrainRplr = &Replayer{
-		SourceName: "livetraindata",
+		SourceName: "mini_parallel_sample_livetraindata",
 		Extractor: extractors.NewMongoExtractor(s.mongo, func(event proto.Message) *eventpb.Event {
 			return &eventpb.Event{Event: &eventpb.Event_LiveTrain{LiveTrain: event.(*livetrainpb.LiveTrainData)}}
 		}, &livetrainpb.LiveTrainData{}, setSortAndID("eventTime", "trainId")), // id is mostly nil so we choose trainId
@@ -117,7 +117,7 @@ func (s *ReplayerServer) Setup(ctx context.Context) error {
 	}
 
 	s.PlannedTrainRplr = &Replayer{
-		SourceName: "plannedtraindata",
+		SourceName: "mini_parallel_sample_plannedtraindata",
 		Extractor: extractors.NewMongoExtractor(s.mongo, func(event proto.Message) *eventpb.Event {
 			return &eventpb.Event{Event: &eventpb.Event_PlannedTrain{PlannedTrain: event.(*plannedtrainpb.PlannedTrainData)}}
 		}, &plannedtrainpb.PlannedTrainData{}, setSortAndID("ingestionTime", "trainId")), // id is mostly nil so we choose trainId
