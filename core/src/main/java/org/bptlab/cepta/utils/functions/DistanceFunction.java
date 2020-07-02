7ae3b80d12486863ba2a59f35ccd9e25df83f63d
Vincent <vincent.opitz@student.hpi.de>
2020-07-02 11:45:49 +0200

    ref remove lines

--- core/src/main/java/org/bptlab/cepta/utils/functions/DistanceFunction.java
+++ core/src/main/java/org/bptlab/cepta/utils/functions/DistanceFunction.java
@@ -4,10 +4,7 @@ import com.google.protobuf.Duration;
 import org.bptlab.cepta.models.internal.correlateable_event.CorrelateableEventOuterClass.CorrelateableEvent;
 
 public interface DistanceFunction{
-
     public double distanceBetween(CorrelateableEvent a, CorrelateableEvent b);
-
     public DistanceFunction setMaxDistance(Number value);
-
     public DistanceFunction setMaxTimespan(Duration value);
 }
